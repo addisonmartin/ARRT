81284745949b7781a77b1d57153fc6026b89dd7c
Addison Martin <addisonejmartin@gmail.com>
2020-09-20 20:46:35 -0400

    Implemented unit vector, cross and dot products.

--- lib/ARRT/math/vector3.rb
+++ lib/ARRT/math/vector3.rb
@@ -26,6 +26,10 @@ class Vector3
     ((@x * @x) + (@y * @y) + (@z * @z))
   end
 
+  def unit_vector
+    self / length
+  end
+
   def -@
     Vector3.new(-1 * @x, -1 * @y, -1 * @z)
   end
@@ -64,6 +68,18 @@ class Vector3
     else
       raise ArgumentError, 'must be a Vector3 or Numeric'
     end
+  end
+
+  def dot(other)
+    ((@x * other.x) + (@y * other.y) + (@z * other.z))
+  end
+
+  def cross(other)
+    Vector3.new(
+      ((@y * other.z) - (@z * other.y)),
+      ((@z * other.x) - (@x * other.z)),
+      ((@x * other.y) - (@y * other.x))
+    )
   end
 
 end
